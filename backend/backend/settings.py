30b2cd18a68f22695b6a93e6cea7dea306dcf00a
Frank Dzikunu <fdzikunu@stu.ucc.edu.gh>
2025-04-06 01:59:36 +0000

    change the database url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,16 +112,32 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'frank',
-        'PASSWORD': '63199510',
-        'HOST': 'localhost',
-        'PORT': '5432',
+import dj_database_url
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Check if DATABASE_URL exists — if yes, use Postgres, else fallback to SQLite
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            'USER': 'frank',
+            'PASSWORD': '63199510',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+
 
 
 # Password validation
