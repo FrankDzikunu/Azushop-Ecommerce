d923e18c3a640deb2817d1e57ed5098c0d055f03
Frank Dzikunu <fdzikunu@stu.ucc.edu.gh>
2025-04-07 10:32:13 +0000

    update setting to load file form cloud

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -190,3 +192,11 @@ if not DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'product_images')
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
